6592fe34131b52bb4ae0b2c98af3235f34ff8a0a
kaiborg <kainoa.accs@gmail.com>
2023-06-14 09:32:04 -1000

    Fixing file structure

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+"use client"
+
+import Link from 'next/link'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Kainoa Borges Portfolio',
+  description: 'Web Developer based in Denver, CO',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* Navbar Div */}
+        <div className='absolute z-10 w-1/2 h-12 items-center'>
+          <ul className='flex ml-8'>
+            <li><Link href='/'><button className='nav-button'>Home</button></Link></li>
+            <li><Link href='/projects'><button className='nav-button'>Projects</button></Link></li>
+            <li><Link href='/contact'><button className='nav-button'>Contact</button></Link></li>
+          </ul>
+        </div>
+        {/* Other Elements */}
+        {children}
+      </body>
+    </html>
+  )
+}
