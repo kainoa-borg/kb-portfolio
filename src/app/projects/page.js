c3d007d8f3df7db62529fe9903235f63156642d4
kaiborg <kainoa.accs@gmail.com>
2023-06-14 18:07:35 -1000

    force rebuild

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -3,7 +3,6 @@
 import {Swiper, SwiperSlide} from 'swiper/react';
 import Image from 'next/image';
 import React from 'react';
-import Link from 'next/link';
 
 
 // import Swiper and modules styles
@@ -25,7 +24,7 @@ export default function ProjectsPage() {
                     <div className='absolute flex flex-col backdrop-blur-xl p-1 items-center justify-center lg:h-[30%] h-[40%] lg:w-1/3 w-[100%] top-[80%] right-[50%] translate-x-[50%] translate-y-[-50%] bg-[#456573]/70'>
                         <h2 className='text-3xl text-center text-white font-mono'>Andrea Vancea Portfolio Site</h2>
                         <h2 className='text-xl text-center text-white font-mono'>Artist portfolio with custom styling</h2>
-                        <Link className='p-4 mt-1 w-1/3 text-center text-white hover:bg-[#62929E] font-mono' href='https://kainoa-borg.github.io/andrea-site'>Explore</Link>
+                        <a className='p-4 mt-1 w-1/3 text-center text-white hover:bg-[#62929E] font-mono' href='https://kainoa-borg.github.io/andrea-site'>Explore</a>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
@@ -33,7 +32,7 @@ export default function ProjectsPage() {
                     <div className='absolute flex flex-col backdrop-blur-xl p-1 items-center justify-center lg:h-[30%] h-[40%] lg:w-1/3 w-[100%] top-[80%] right-[50%] translate-x-[50%] translate-y-[-50%] bg-[#456573]/70'>
                         <h2 className='text-3xl z-10 text-center text-white font-mono'>Food Forward Tracker</h2>
                         <h2 className='text-xl z-10 text-center text-white font-mono'>Full-featured management software for a meal-planning company</h2>
-                        <Link className='p-4 mt-1 w-1/3 text-center text-white hover:bg-[#62929E] font-mono' href='https://github.com/FoodDesertDatabase/FoodForwardTracker/'>View on Github</Link>
+                        <a className='p-4 mt-1 w-1/3 text-center text-white hover:bg-[#62929E] font-mono' href='https://github.com/FoodDesertDatabase/FoodForwardTracker/'>View on Github</a>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
@@ -41,7 +40,7 @@ export default function ProjectsPage() {
                     <div className='absolute flex flex-col backdrop-blur-xl p-1 items-center justify-center lg:h-[30%] h-[40%] lg:w-1/3 w-[100%] top-[80%] right-[50%] translate-x-[50%] translate-y-[-50%] bg-[#456573]/70'>
                         <h2 className='text-3xl z-10 text-center text-white font-mono'>Hacker News Clone</h2>
                         <h2 className='text-xl z-10 text-center text-white font-mono'>Functional clone of popular tech news site</h2>
-                        <Link className='p-4 mt-1 w-1/3 text-center text-white hover:bg-[#62929E] font-mono' href='https://kainoa-borg.github.io/hacker-news-clone'>Explore</Link>
+                        <a className='p-4 mt-1 w-1/3 text-center text-white hover:bg-[#62929E] font-mono' href='https://kainoa-borg.github.io/hacker-news-clone'>Explore</a>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
@@ -49,7 +48,7 @@ export default function ProjectsPage() {
                     <div className='absolute flex flex-col backdrop-blur-xl p-1 items-center justify-center lg:h-[30%] h-[40%] lg:w-1/3 w-[100%] top-[80%] right-[50%] translate-x-[50%] translate-y-[-50%] bg-[#456573]/70'>
                         <h2 className='text-3xl z-10 text-center text-white font-mono'>Multi-Threaded Raytracing Project</h2>
                         <h2 className='text-xl z-10 text-center text-white font-mono'>CPU-parallel raytracer built with C++</h2>
-                        <Link className='p-4 mt-1 w-1/3 text-center text-white hover:bg-[#62929E] font-mono' href='https://github.com/kainoa-borg/Simple-Multithreaded-Raytracer'>View on Github</Link>
+                        <a className='p-4 mt-1 w-1/3 text-center text-white hover:bg-[#62929E] font-mono' href='https://github.com/kainoa-borg/Simple-Multithreaded-Raytracer'>View on Github</a>
                     </div>
                 </SwiperSlide>
             </Swiper>
