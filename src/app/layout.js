9e453a646d5b074408c8930f05766c448f37f7c2
kaiborg <kainoa.accs@gmail.com>
2023-06-14 16:05:03 -1000

    Fix build error

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,14 +6,18 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Kainoa Borges Portfolio',
-  description: 'Web Developer based in Denver, CO',
-}
+// export const metadata = {
+//   title: 'Kainoa Borges Portfolio',
+//   description: 'Web Developer based in Denver, CO',
+// }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Kainoa Borges Dev</title>
+        <meta name='descriptions' content='Software Developer based in Denver, CO'></meta>
+      </head>
       <body className={inter.className}>
         {/* Navbar Div */}
         <div className='absolute z-10 w-1/2 h-12 items-center bg-[#456573]'>
