0479f07a28d56982b41204b2fdd88f36e09e2842
kaiborg <kainoa.accs@gmail.com>
2023-06-14 16:13:36 -1000

    Fix build error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+      },
+      images: {
+        unoptimized: true,
+      },
     output: 'export',
-    images: {
-        unoptimized: true
-    }
 }
 
 module.exports = nextConfig
